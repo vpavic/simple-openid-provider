3ce4155383d5c032220e9c51cad8c699f8eb3257
Vedran Pavic <vedran.pavic@gmail.com>
2017-07-11 08:27:29 +0200

    Initial commit

---
+++ src/main/java/io/github/vpavic/NimbusOIDCProviderApplication.java
@@ -0,0 +1,13 @@
+package io.github.vpavic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimbusOIDCProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimbusOIDCProviderApplication.class, args);
+	}
+
+}
