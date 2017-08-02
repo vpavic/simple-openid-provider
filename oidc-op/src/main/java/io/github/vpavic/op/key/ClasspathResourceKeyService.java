19917694150ac03ae51b40af488dc15cb9579a6c
Vedran Pavic <vedran.pavic@gmail.com>
2017-08-02 09:38:07 +0200

    Simplify `KeyService`

--- oidc-op/src/main/java/io/github/vpavic/op/key/ClasspathResourceKeyService.java
+++ oidc-op/src/main/java/io/github/vpavic/op/key/ClasspathResourceKeyService.java
@@ -21,21 +21,6 @@ public class ClasspathResourceKeyService implements KeyService {
 	}
 
 	@Override
-	public JWK generateKey(String kid) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void save(JWK jwk) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public JWK findByKeyId(String kid) {
-		return this.jwkSet.getKeyByKeyId(kid);
-	}
-
-	@Override
 	public JWK findDefault() {
 		return this.jwkSet.getKeyByKeyId(DEFAULT_KID);
 	}
--- oidc-op/src/main/java/io/github/vpavic/op/key/KeyService.java
+++ oidc-op/src/main/java/io/github/vpavic/op/key/KeyService.java
@@ -6,12 +6,6 @@ import com.nimbusds.jose.jwk.JWK;
 
 public interface KeyService {
 
-	JWK generateKey(String kid);
-
-	void save(JWK jwk);
-
-	JWK findByKeyId(String kid);
-
 	JWK findDefault();
 
 	List<JWK> findAll();
