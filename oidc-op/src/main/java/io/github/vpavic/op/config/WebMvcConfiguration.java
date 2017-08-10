76e7168a706018159376490925f3542a49302b0f
Vedran Pavic <vedran.pavic@gmail.com>
2017-08-10 08:26:45 +0200

    Improve home page

--- oidc-op/src/main/java/io/github/vpavic/op/config/WebMvcConfiguration.java
+++ oidc-op/src/main/java/io/github/vpavic/op/config/WebMvcConfiguration.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
