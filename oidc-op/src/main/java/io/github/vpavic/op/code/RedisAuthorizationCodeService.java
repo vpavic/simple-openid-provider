26564dcdb7d6045bd5e3e027bbba2a12a19d5420
Vedran Pavic <vedran.pavic@gmail.com>
2017-08-10 23:07:23 +0200

    Replace Hazelcast with Redis

---
+++ oidc-op/src/main/java/io/github/vpavic/op/code/RedisAuthorizationCodeService.java
@@ -0,0 +1,57 @@
+package io.github.vpavic.op.code;
+
+import java.util.concurrent.TimeUnit;
+
+import com.nimbusds.oauth2.sdk.AuthorizationCode;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisAuthorizationCodeService implements AuthorizationCodeService {
+
+	private static final String PREFIX = "oidc:op:codes";
+
+	private final RedisOperations<String, Object> redisOperations;
+
+	public RedisAuthorizationCodeService(RedisConnectionFactory connectionFactory) {
+		this.redisOperations = createRedisOperations(connectionFactory);
+	}
+
+	@Override
+	public AuthorizationCode create(AuthorizationCodeContext context) {
+		AuthorizationCode code = new AuthorizationCode();
+		this.redisOperations.boundValueOps(getKey(code)).set(context, 10, TimeUnit.MINUTES);
+		return code;
+	}
+
+	@Override
+	public AuthorizationCodeContext consume(AuthorizationCode code) {
+		String key = getKey(code);
+		AuthorizationCodeContext context = (AuthorizationCodeContext) this.redisOperations.boundValueOps(key).get();
+
+		if (context == null) {
+			throw new IllegalArgumentException("Invalid code " + code);
+		}
+
+		this.redisOperations.delete(key);
+		return context;
+	}
+
+	private static String getKey(AuthorizationCode code) {
+		return PREFIX + code.getValue();
+	}
+
+	private RedisOperations<String, Object> createRedisOperations(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(connectionFactory);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setDefaultSerializer(new JdkSerializationRedisSerializer(getClass().getClassLoader()));
+		redisTemplate.afterPropertiesSet();
+		return redisTemplate;
+	}
+
+}
