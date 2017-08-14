da1a776fb43ce0e2b3965360a4e6c8ebf3b80628
Vedran Pavic <vedran.pavic@gmail.com>
2017-08-14 08:40:38 +0200

    Improve cloud configuration

--- oidc-op/src/main/java/io/github/vpavic/op/config/CloudConfiguration.java
+++ oidc-op/src/main/java/io/github/vpavic/op/config/CloudConfiguration.java
@@ -3,6 +3,9 @@ package io.github.vpavic.op.config;
 import javax.sql.DataSource;
 
 import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.PooledServiceConnectorConfig;
+import org.springframework.cloud.service.PooledServiceConnectorConfig.PoolConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,12 +17,16 @@ public class CloudConfiguration extends AbstractCloudConfig {
 
 	@Bean
 	public DataSource dataSource() {
-		return connectionFactory().dataSource();
+		PoolConfig poolConfig = new PoolConfig(4, 4, 3000);
+		DataSourceConfig dataSourceConfig = new DataSourceConfig(poolConfig, null);
+		return connectionFactory().dataSource(dataSourceConfig);
 	}
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return connectionFactory().redisConnectionFactory();
+		PoolConfig poolConfig = new PoolConfig(5, 30, 3000);
+		PooledServiceConnectorConfig redisConnectionFactoryConfig = new PooledServiceConnectorConfig(poolConfig);
+		return connectionFactory().redisConnectionFactory(redisConnectionFactoryConfig);
 	}
 
 }
