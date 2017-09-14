dd2f6083209861f768168c38929a8c94d76fe0d1
Vedran Pavic <vedran.pavic@gmail.com>
2017-09-14 23:16:49 +0200

    Use embedded database and container session store by default

--- oidc-op/src/main/java/io/github/vpavic/op/config/SessionConfiguration.java
+++ oidc-op/src/main/java/io/github/vpavic/op/config/SessionConfiguration.java
@@ -1,5 +1,6 @@
 package io.github.vpavic.op.config;
 
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.web.http.CookieSerializer;
@@ -8,11 +9,20 @@ import org.springframework.session.web.http.DefaultCookieSerializer;
 @Configuration
 public class SessionConfiguration {
 
+	private final ServerProperties properties;
+
+	public SessionConfiguration(ServerProperties properties) {
+		this.properties = properties;
+	}
+
 	@Bean
 	public CookieSerializer cookieSerializer() {
+		ServerProperties.Session.Cookie sessionCookie = this.properties.getSession().getCookie();
+
 		DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
-		cookieSerializer.setCookieName("sid");
-		cookieSerializer.setUseHttpOnlyCookie(false);
+		cookieSerializer.setCookieName(sessionCookie.getName());
+		cookieSerializer.setUseHttpOnlyCookie(sessionCookie.getHttpOnly());
+
 		return cookieSerializer;
 	}
 
