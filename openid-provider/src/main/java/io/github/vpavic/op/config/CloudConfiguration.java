d01877b7ca33375eea3d62d336e3b7e1450c47b1
Vedran Pavic <vedran.pavic@gmail.com>
2017-09-22 14:35:24 +0200

    Replace Redis with Hazelcast

--- openid-provider/src/main/java/io/github/vpavic/op/config/CloudConfiguration.java
+++ openid-provider/src/main/java/io/github/vpavic/op/config/CloudConfiguration.java
@@ -3,13 +3,11 @@ package io.github.vpavic.op.config;
 import javax.sql.DataSource;
 
 import org.springframework.cloud.config.java.AbstractCloudConfig;
-import org.springframework.cloud.service.PooledServiceConnectorConfig;
 import org.springframework.cloud.service.PooledServiceConnectorConfig.PoolConfig;
 import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
 @Profile("cloud")
@@ -20,13 +18,6 @@ public class CloudConfiguration extends AbstractCloudConfig {
 		PoolConfig poolConfig = new PoolConfig(4, 4, 3000);
 		DataSourceConfig dataSourceConfig = new DataSourceConfig(poolConfig, null);
 		return connectionFactory().dataSource(dataSourceConfig);
-	}
-
-	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		PoolConfig poolConfig = new PoolConfig(5, 30, 3000);
-		PooledServiceConnectorConfig redisConnectionFactoryConfig = new PooledServiceConnectorConfig(poolConfig);
-		return connectionFactory().redisConnectionFactory(redisConnectionFactoryConfig);
 	}
 
 }
---
+++ openid-provider/src/main/java/io/github/vpavic/op/oauth2/code/HazelcastAuthorizationCodeService.java
@@ -0,0 +1,51 @@
+package io.github.vpavic.op.oauth2.code;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.nimbusds.oauth2.sdk.AuthorizationCode;
+import com.nimbusds.oauth2.sdk.GeneralException;
+import com.nimbusds.oauth2.sdk.OAuth2Error;
+import org.springframework.stereotype.Service;
+
+import io.github.vpavic.op.config.OpenIdProviderProperties;
+
+@Service
+public class HazelcastAuthorizationCodeService implements AuthorizationCodeService {
+
+	private static final String CODES_MAP = "op:authorizationCodes";
+
+	private final OpenIdProviderProperties properties;
+
+	private final IMap<String, AuthorizationCodeContext> codes;
+
+	public HazelcastAuthorizationCodeService(OpenIdProviderProperties properties, HazelcastInstance hazelcastInstance) {
+		Objects.requireNonNull(properties, "properties must not be null");
+		Objects.requireNonNull(hazelcastInstance, "hazelcastInstance must not be null");
+
+		this.properties = properties;
+		this.codes = hazelcastInstance.getMap(CODES_MAP);
+	}
+
+	@Override
+	public AuthorizationCode create(AuthorizationCodeContext context) {
+		AuthorizationCode code = new AuthorizationCode();
+		this.codes.put(code.getValue(), context, this.properties.getCode().getLifetime(), TimeUnit.SECONDS);
+
+		return code;
+	}
+
+	@Override
+	public AuthorizationCodeContext consume(AuthorizationCode code) throws GeneralException {
+		AuthorizationCodeContext context = this.codes.remove(code.getValue());
+
+		if (context == null) {
+			throw new GeneralException(OAuth2Error.INVALID_GRANT);
+		}
+
+		return context;
+	}
+
+}
