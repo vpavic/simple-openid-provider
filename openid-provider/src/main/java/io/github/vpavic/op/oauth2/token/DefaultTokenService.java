514da31b44e9e38c3243b007fb1072b73e013cf8
Vedran Pavic <vedran.pavic@gmail.com>
2017-10-05 10:30:48 +0200

    Refactor `TokenService` to support multiple JWAs

--- openid-provider/src/main/java/io/github/vpavic/op/oauth2/token/DefaultTokenService.java
+++ openid-provider/src/main/java/io/github/vpavic/op/oauth2/token/DefaultTokenService.java
@@ -4,16 +4,16 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.UUID;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.KeyException;
 import com.nimbusds.jose.KeySourceException;
-import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKMatcher;
 import com.nimbusds.jose.jwk.JWKSelector;
@@ -21,14 +21,17 @@ import com.nimbusds.jose.jwk.KeyType;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWT;
-import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.oauth2.sdk.AuthorizationCode;
 import com.nimbusds.oauth2.sdk.ParseException;
 import com.nimbusds.oauth2.sdk.Scope;
+import com.nimbusds.oauth2.sdk.assertions.jwt.JWTAssertionDetails;
+import com.nimbusds.oauth2.sdk.assertions.jwt.JWTAssertionFactory;
+import com.nimbusds.oauth2.sdk.auth.Secret;
 import com.nimbusds.oauth2.sdk.id.Audience;
 import com.nimbusds.oauth2.sdk.id.ClientID;
 import com.nimbusds.oauth2.sdk.id.Issuer;
+import com.nimbusds.oauth2.sdk.id.JWTID;
 import com.nimbusds.oauth2.sdk.id.Subject;
 import com.nimbusds.oauth2.sdk.token.AccessToken;
 import com.nimbusds.oauth2.sdk.token.BearerAccessToken;
@@ -47,11 +50,11 @@ import io.github.vpavic.op.oauth2.jwk.JwkSetService;
 import io.github.vpavic.op.oauth2.userinfo.UserInfoMapper;
 
 @Service
-public class TokenServiceImpl implements TokenService {
+public class DefaultTokenService implements TokenService {
 
 	private static final String SCOPE_CLAIM = "scope";
 
-	private static final JWSAlgorithm jwsAlgorithm = JWSAlgorithm.RS256;
+	private static final JWSAlgorithm defaultAlgorithm = JWSAlgorithm.RS256;
 
 	private final OpenIdProviderProperties properties;
 
@@ -59,7 +62,7 @@ public class TokenServiceImpl implements TokenService {
 
 	private final RefreshTokenStore refreshTokenStore;
 
-	public TokenServiceImpl(OpenIdProviderProperties properties, JwkSetService jwkSetService,
+	public DefaultTokenService(OpenIdProviderProperties properties, JwkSetService jwkSetService,
 			RefreshTokenStore refreshTokenStore) {
 		Objects.requireNonNull(properties, "properties must not be null");
 		Objects.requireNonNull(jwkSetService, "jwkSetService must not be null");
@@ -72,55 +75,42 @@ public class TokenServiceImpl implements TokenService {
 
 	@Override
 	public AccessToken createAccessToken(AccessTokenRequest accessTokenRequest) {
-		Instant issuedAt = Instant.now();
+		Instant now = Instant.now();
 		int tokenLifetime = this.properties.getAccessToken().getLifetime();
-
-		JWK jwk = resolveJwk(JWSAlgorithm.RS256);
-
-		// @formatter:off
-		JWSHeader header = new JWSHeader.Builder(jwsAlgorithm)
-				.keyID(jwk.getKeyID())
-				.build();
-		// @formatter:on
 
 		String principal = accessTokenRequest.getPrincipal();
 		Scope scope = accessTokenRequest.getScope();
+		AccessTokenClaimsMapper accessTokenClaimsMapper = accessTokenRequest.getAccessTokenClaimsMapper();
 
-		List<String> audience = new ArrayList<>();
-		audience.add(this.properties.getIssuer());
+		Issuer issuer = new Issuer(this.properties.getIssuer());
+		Subject subject = new Subject(principal);
+		List<Audience> audience = new ArrayList<>();
+		audience.add(new Audience(this.properties.getIssuer()));
 
 		for (Scope.Value value : scope) {
 			String resource = this.properties.getAuthorization().getResourceScopes().get(value.getValue());
 
 			if (resource != null) {
-				audience.add(resource);
+				audience.add(new Audience(resource));
 			}
 		}
 
-		// @formatter:off
-		JWTClaimsSet.Builder claimsSetBuilder = new JWTClaimsSet.Builder()
-				.issuer(this.properties.getIssuer())
-				.subject(principal)
-				.audience(audience)
-				.expirationTime(Date.from(issuedAt.plusSeconds(tokenLifetime)))
-				.issueTime(Date.from(issuedAt))
-				.jwtID(UUID.randomUUID().toString())
-				.claim(SCOPE_CLAIM, scope.toString());
-		// @formatter:on
-
-		AccessTokenClaimsMapper accessTokenClaimsMapper = accessTokenRequest.getAccessTokenClaimsMapper();
+		Date expirationTime = Date.from(now.plusSeconds(tokenLifetime));
+		Date issueTime = Date.from(now);
+		JWTID jwtId = new JWTID();
+		Map<String, Object> claims = new HashMap<>();
+		claims.put(SCOPE_CLAIM, scope);
 
 		if (accessTokenClaimsMapper != null) {
-			Map<String, Object> claims = accessTokenClaimsMapper.map(principal);
-			claims.forEach(claimsSetBuilder::claim);
+			claims.putAll(accessTokenClaimsMapper.map(principal));
 		}
 
-		JWTClaimsSet claimsSet = claimsSetBuilder.build();
-
 		try {
-			SignedJWT accessToken = new SignedJWT(header, claimsSet);
-			RSASSASigner signer = new RSASSASigner((RSAKey) jwk);
-			accessToken.sign(signer);
+			JWTAssertionDetails details = new JWTAssertionDetails(issuer, subject, audience, expirationTime, null,
+					issueTime, jwtId, claims);
+			RSAKey rsaKey = (RSAKey) resolveJwk(defaultAlgorithm);
+			SignedJWT accessToken = JWTAssertionFactory.create(details, defaultAlgorithm, rsaKey.toRSAPrivateKey(),
+					rsaKey.getKeyID(), null);
 
 			return new BearerAccessToken(accessToken.serialize(), tokenLifetime, scope);
 		}
@@ -131,17 +121,17 @@ public class TokenServiceImpl implements TokenService {
 
 	@Override
 	public RefreshToken createRefreshToken(RefreshTokenRequest refreshTokenRequest) {
+		Instant now = Instant.now();
 		Scope scope = refreshTokenRequest.getScope();
 
 		if (!scope.contains(OIDCScopeValue.OFFLINE_ACCESS)) {
 			throw new IllegalArgumentException("Scope '" + OIDCScopeValue.OFFLINE_ACCESS + "' is required");
 		}
 
-		Instant issuedAt = Instant.now();
 		int tokenLifetime = this.properties.getRefreshToken().getLifetime();
 
 		RefreshToken refreshToken = new RefreshToken();
-		Instant expiry = (tokenLifetime > 0) ? issuedAt.plusSeconds(tokenLifetime) : null;
+		Instant expiry = (tokenLifetime > 0) ? now.plusSeconds(tokenLifetime) : null;
 		RefreshTokenContext context = new RefreshTokenContext(refreshTokenRequest.getPrincipal(),
 				refreshTokenRequest.getClientID(), scope, expiry);
 		this.refreshTokenStore.save(refreshToken, context);
@@ -151,29 +141,24 @@ public class TokenServiceImpl implements TokenService {
 
 	@Override
 	public JWT createIdToken(IdTokenRequest idTokenRequest) {
+		Instant now = Instant.now();
 		Scope scope = idTokenRequest.getScope();
 
 		if (!scope.contains(OIDCScopeValue.OPENID)) {
 			throw new IllegalArgumentException("Scope '" + OIDCScopeValue.OPENID + "' is required");
 		}
 
-		Instant issuedAt = Instant.now();
-
-		JWK jwk = resolveJwk(JWSAlgorithm.RS256);
-
-		// @formatter:off
-		JWSHeader header = new JWSHeader.Builder(jwsAlgorithm)
-				.keyID(jwk.getKeyID())
-				.build();
-		// @formatter:on
-
 		String principal = idTokenRequest.getPrincipal();
 		ClientID clientID = idTokenRequest.getClientID();
+		JWSAlgorithm algorithm = defaultAlgorithm;
 
-		IDTokenClaimsSet claimsSet = new IDTokenClaimsSet(new Issuer(this.properties.getIssuer()),
-				new Subject(principal), Audience.create(clientID.getValue()),
-				Date.from(issuedAt.plusSeconds(this.properties.getIdToken().getLifetime())), Date.from(issuedAt));
+		Issuer issuer = new Issuer(this.properties.getIssuer());
+		Subject subject = new Subject(principal);
+		List<Audience> audience = Audience.create(clientID.getValue());
+		Date expirationTime = Date.from(now.plusSeconds(this.properties.getIdToken().getLifetime()));
+		Date issueTime = Date.from(now);
 
+		IDTokenClaimsSet claimsSet = new IDTokenClaimsSet(issuer, subject, audience, expirationTime, issueTime);
 		claimsSet.setAuthenticationTime(Date.from(idTokenRequest.getAuthenticationTime()));
 		claimsSet.setNonce(idTokenRequest.getNonce());
 		claimsSet.setACR(idTokenRequest.getAcr());
@@ -181,19 +166,22 @@ public class TokenServiceImpl implements TokenService {
 		claimsSet.setAuthorizedParty(new AuthorizedParty(clientID.getValue()));
 
 		if (this.properties.getFrontChannelLogout().isEnabled()) {
-			claimsSet.setSessionID(new SessionID(idTokenRequest.getSessionId()));
+			SessionID sessionId = new SessionID(idTokenRequest.getSessionId());
+			claimsSet.setSessionID(sessionId);
 		}
 
 		AccessToken accessToken = idTokenRequest.getAccessToken();
 
 		if (accessToken != null) {
-			claimsSet.setAccessTokenHash(AccessTokenHash.compute(accessToken, jwsAlgorithm));
+			AccessTokenHash accessTokenHash = AccessTokenHash.compute(accessToken, algorithm);
+			claimsSet.setAccessTokenHash(accessTokenHash);
 		}
 
 		AuthorizationCode code = idTokenRequest.getCode();
 
 		if (code != null) {
-			claimsSet.setCodeHash(CodeHash.compute(code, jwsAlgorithm));
+			CodeHash codeHash = CodeHash.compute(code, algorithm);
+			claimsSet.setCodeHash(codeHash);
 		}
 
 		IdTokenClaimsMapper idTokenClaimsMapper = idTokenRequest.getIdTokenClaimsMapper();
@@ -211,18 +199,33 @@ public class TokenServiceImpl implements TokenService {
 		}
 
 		try {
-			SignedJWT idToken = new SignedJWT(header, claimsSet.toJWTClaimsSet());
-			RSASSASigner signer = new RSASSASigner((RSAKey) jwk);
-			idToken.sign(signer);
+			JWTAssertionDetails details = JWTAssertionDetails.parse(claimsSet.toJWTClaimsSet());
 
-			return idToken;
+			if (JWSAlgorithm.Family.HMAC_SHA.contains(algorithm)) {
+				Secret secret = new Secret(); // TODO client secret
+
+				return JWTAssertionFactory.create(details, algorithm, secret);
+			}
+			else if (JWSAlgorithm.Family.RSA.contains(algorithm)) {
+				RSAKey rsaKey = (RSAKey) resolveJwk(algorithm);
+
+				return JWTAssertionFactory.create(details, algorithm, rsaKey.toRSAPrivateKey(), rsaKey.getKeyID(),
+						null);
+			}
+			else if (JWSAlgorithm.Family.EC.contains(algorithm)) {
+				ECKey ecKey = (ECKey) resolveJwk(algorithm);
+
+				return JWTAssertionFactory.create(details, algorithm, ecKey.toECPrivateKey(), ecKey.getKeyID(), null);
+			}
+
+			throw new KeyException("Unsupported algorithm: " + algorithm);
 		}
 		catch (ParseException | JOSEException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	private JWK resolveJwk(JWSAlgorithm algorithm) {
+	private JWK resolveJwk(JWSAlgorithm algorithm) throws KeySourceException {
 		// @formatter:off
 		JWKMatcher jwkMatcher = new JWKMatcher.Builder()
 				.keyType(KeyType.forAlgorithm(algorithm))
@@ -231,14 +234,7 @@ public class TokenServiceImpl implements TokenService {
 		// @formatter:on
 
 		JWKSelector jwkSelector = new JWKSelector(jwkMatcher);
-		List<JWK> jwks;
-
-		try {
-			jwks = this.jwkSetService.get(jwkSelector, null);
-		}
-		catch (KeySourceException e) {
-			throw new RuntimeException(e);
-		}
+		List<JWK> jwks = this.jwkSetService.get(jwkSelector, null);
 
 		return jwks.iterator().next();
 	}
